099b649abff902dd882f2ff21e054c2b0b6b0724
kwavey <kbirongo@gmail.com>
2021-11-03 10:28:59 +0100

    changing image

--- chessboard/js/loadboard.js
+++ chessboard/js/loadboard.js
@@ -1,7 +1,7 @@
-$(function() {
+function initBoard() {
     var cfg = {
-        pieceTheme: 'https://koblenski.github.io/javascript/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png',
+        pieceTheme: 'img/chesspieces/wikipedia/{piece}.png',
         position: 'start'
     };
     var board = ChessBoard('board1', cfg);
-});
+};
